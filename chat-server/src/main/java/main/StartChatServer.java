cf83a7e7169e68d479ae0faa8043ec2f812e35d7
akiyo <akiyoshimoto@hotmail.com>
2021-08-16 23:43:17 -0500

    Added .metadata file

--- chat-server/src/main/java/main/StartChatServer.java
+++ chat-server/src/main/java/main/StartChatServer.java
@@ -7,9 +7,14 @@ import server.ChatServer;
 public class StartChatServer {
 
 	public static void main(String[] args) {
+		int port = 8000;
+		
+		if (args.length == 1) {
+			port = Integer.parseInt(args[0]);
+		}
 		ChatServer chatServer = null;
 		try {
-			chatServer = new ChatServer(8000);
+			chatServer = new ChatServer(port);
 		} catch (IOException e) {
 			System.out.println("Failed to start chat server");
 			e.printStackTrace();
